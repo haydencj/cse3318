31a02629e2facfe4774ca8fa5b8a316182ce77ac
Hayden Johnson <haydenjohnson@HaydenMBPro.tx.rr.com>
2022-10-09 16:39:00 -0500

    hw4 file creation

---
+++ hw4/heap.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "heap.h"  
+
+#define DEBUG 1
+
+/*
+struct heap_struct make_heap_empty(int cap){
+}
+
+struct heap_struct make_heap(int N, int * arr){
+}
+
+void destroy(struct heap_struct * heapP){
+}
+
+
+void print_heap(struct heap_struct heapS){
+}
+
+
+void swim_up(int idx, int * arr){
+}
+
+
+void sink_down(int i, int N, int * arr){
+}
+
+void add(struct heap_struct * heapP, int new_item){
+}
+
+int peek(struct heap_struct heapS){
+}
+
+
+int poll(struct heap_struct * heapP){
+}
+
+*/
