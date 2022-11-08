71b031f0da10d2acfa20bb615922f138644bdc09
Hayden Johnson <haydenjohns02@gmail.com>
2022-11-08 11:04:10 -0600

    hw6 start

---
+++ hw6/spell.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "spell.h"
+
+/*  Suggestions
+- When you work with 2D arrays, be careful. Either manage the memory yourself, or
+work with local 2D arrays. Note C99 allows parameters as array sizes as long as 
+they are declared before the array in the parameter list. See: 
+https://www.geeksforgeeks.org/pass-2d-array-parameter-c/
+
+Worst case time complexity to compute the edit distance from T test words
+ to D dictionary words where all words have length MAX_LEN:
+Student answer:  Theta(............)
+
+
+Worst case to do an unsuccessful binary search in a dictionary with D words, when 
+all dictionary words and the searched word have length MAX_LEN 
+Student answer:  Theta(............)
+*/
+
+
+/* You can write helper functions here */
+
+
+/*
+Parameters:
+first_string - pointer to the first string (displayed vertical in the table)
+second_string - pointer to the second string (displayed horizontal in the table)
+print_table - If 1, the table with the calculations for the edit distance will be printed.
+              If 0 (or any value other than 1)it will not print the table
+(See sample run)
+Return:  the value of the edit distance (e.g. 3)
+*/
+int edit_distance(char * first_string, char * second_string, int print_table){
+	return -1;  // replace this line with your code
+}
+	  
+
+/*
+Parameters:
+testname - string containing the name of the file with the paragraph to spell check, includes .txt e.g. "text1.txt" 
+dictname - name of file with dictionary words. Includes .txt, e.g. "dsmall.txt"
+printOn - If 1 it will print EXTRA debugging/tracing information (in addition to what it prints when 0):
+			 dictionary as read from the file	
+			 dictionary AFTER sorting in lexicographical order
+			 for every word searched in the dictionary using BINARY SEARCH shows all the "probe" words and their indices indices
+			 See sample run.
+	      If 0 (or anything other than 1) does not print the above information, but still prints the number of probes.
+		     See sample run.
+*/
+void spell_check(char * testname, char * dictname, int printOn){
+	// Write your code here
+}
+
