d14895344893bc651dd2d9dce19a4e524f1f24f8
Hayden Johnson <haydenjohns02@gmail.com>
2022-11-09 11:13:10 -0600

    hw6 0.2

--- hw6/spell.c
+++ hw6/spell.c
@@ -22,14 +22,73 @@ Student answer:  Theta(............)
 
 
 /* You can write helper functions here */
-int calc_dist(char* first, char* second, int i, int j, int** table) {
+char** test_to_array(char * filename) {
+	FILE* fp = fopen(filename, "r");
+	if(!fp) {
+		printf("Test file could not be opened.\n");
+		return NULL;
+	}
+
+	char* token;
+	char line[100];
+	int i = 0;
+
+}
+
+char** dict_to_array(char * filename, int* size) {
+	FILE* fp = fopen(filename, "r");
+	if(!fp) {
+		printf("Dictionary could not be opened.\n");
+		return NULL;
+	}
+
+	char* token;
+	char line[100];
+	int i = 0;
+	
+	//get size of dict from file
+	fgets(line, 100, fp);
+	strtok(line, "\n"); 
+	*size = atoi(line);
+
+	//create 2d arr
+	char dict_arr[*size][100];
+
+	//get each token and put in arr
+	for(i = 0; i < *size; i++) {
+		fgets(line, 100, fp);
+		dict_arr[i] = token;
+	}
+
+	fclose(fp);
+	return dict_arr;
+}
+
+int min(int a, int b, int c){
+	int min;
+	if( (a<b) && (a<c) )
+	{
+		min = a;
+	}
+	else if(b<c)
+	{
+		min = b;
+	}
+	else
+	{
+		min = c;
+	}
+	return min;
+}
+
+int calc_dist(char* first, char* second, int i, int j, int rows, int cols, int table[][cols]) {
 	if(i == 0 && j == 0) return 0;
 	else if(i == 0) return j;
 	else if(j == 0) return i;
 	else {
 		if(first[i-1] == second[j-1]) return table[i-1][j-1];
 		else {
-			return min(table[i-1][j],table[i][j-1],table[i-1][j-1]);
+			return min(table[i-1][j]+1,table[i][j-1]+1,table[i-1][j-1]+1);
 		}
 	}
 }
@@ -48,16 +107,21 @@ int edit_distance(char * first_string, char * second_string, int print_table){
 	int cols = strlen(second_string) + 1;
 	int i, j;
 	int table[rows][cols];
-	int dist;
-
+	
 	for(i=0; i<rows; i++) {
 		for(j=0; j<cols; j++) {
-			table[i][j] = calc_dist(first_string, second_string, i, j, table);
+			table[i][j] = calc_dist(first_string, second_string, i, j, rows, cols, table);
 		}
 	}
 
 	if(print_table == 1) {
-
+		for (i = 0; i < rows; i++) {
+			printf("%c ", first_string[i]);
+        	for (j = 0; j < cols; j++) {
+            	printf("%d ", table[i][j]);
+        	}
+        	printf("\n");
+    	}
 	}
 
 	return table[rows-1][cols-1];  // final edit distance, bottom right of table
@@ -77,6 +141,24 @@ printOn - If 1 it will print EXTRA debugging/tracing information (in addition to
 		     See sample run.
 */
 void spell_check(char * testname, char * dictname, int printOn){
-	// Write your code here
+	int dict_size = 0;
+	int* dict_ptr = &dict_ptr;
+
+	//char** test_arr = file_to_array(testname);
+	printf("Loading the dictionary file: %s\n", dictname);
+	char** dict_arr = dict_to_array(dictname, dict_ptr);
+	printf("Dictionary has size: %d", dict_size);
+
+	for(int i = 0; i<dict_size; i++){
+		printf("%s\n", dict_arr[i]);
+	}
+
+	if(printOn == 1) {
+
+	}
+
+	else {
+
+	}
 }
 
